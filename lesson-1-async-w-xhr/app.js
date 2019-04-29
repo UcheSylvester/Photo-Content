03a49fdd589474127bca1c0e78d768b031f1777e
Uchenna <okorocode@gmail.com>
2019-04-29 23:44:16 +0100

    setting request header for unsplash

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -10,5 +10,19 @@
         searchedForText = searchField.value;
     });
 
-    console.log(searchField)
+    searchedForText = 'hippos';
+
+    // onload function for success
+    function addImage() {
+        console.log('working!')
+    }
+
+    const unsplashRequest = new XMLHttpRequest();
+    unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+    unsplashRequest.onload = addImage;
+
+    // setting request header for authorization
+    unsplashRequest.setRequestHeader('Authorization', 'Client-ID 25b73c89-86b9-4b40-b447-04657a3149fa');
+    unsplashRequest.send();
+
 })();
