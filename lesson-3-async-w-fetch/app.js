c219d4a88e969b1e78dd8f0781bbb950904c15f8
Uchenna <okorocode@gmail.com>
2019-05-05 17:06:29 +0100

    fetched image results!

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -8,5 +8,43 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const url = `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`;
+        const myInit = {
+            headers: {authorization: 'Client-ID ddaefa4c92ec5deae8988523e03426a592933e19724005cc626934743cf88c80'}
+        };
+
+        fetch(url, myInit)
+            .then(response=> response.json())
+            .then(addImage)
+            .catch(e => requestError(e, 'image'))
+
+        // adding Image to the page
+        function addImage(data) {
+            let htmlContent = '';
+            const firstImage = data.results[0];
+
+            if(firstImage) {
+                htmlContent = `
+                <figure>
+                    <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                    <figcaption>${searchedForText}</figcaption>
+                </figure>
+                `
+            } else {
+                htmlContent = `Oppsss!! There is no image available`
+            }
+
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }
+
+        function requestError(e, part) {
+            console.log(e.message)
+
+            let errorMessage = `<p class="network-warning">Oh... no! <em>${e.message}</em> error occurred on ${part} request for ${searchedForText}. Please check network connections and try again!`
+            responseContainer.insertAdjacentHTML('beforeend', errorMessage);
+        }
+
+
     });
 })();
