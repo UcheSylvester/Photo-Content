25ebea029830851183e60330a13fe95058f49013
Uchenna <okorocode@gmail.com>
2019-05-05 17:40:50 +0100

    fetched articles!

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -9,12 +9,15 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
 
-        const url = `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`;
-        const myInit = {
+        /*** FETCHING IMAGES FROM UNSPLASH ***/
+
+        const imageUrl = `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`;
+        const imageObject = {
             headers: {authorization: 'Client-ID ddaefa4c92ec5deae8988523e03426a592933e19724005cc626934743cf88c80'}
         };
 
-        fetch(url, myInit)
+
+        fetch(imageUrl, imageObject)
             .then(response=> response.json())
             .then(addImage)
             .catch(e => requestError(e, 'image'))
@@ -38,6 +41,37 @@
             responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
         }
 
+
+        /**** FETCHING ATICLES FOR SEARCH */
+
+        const articlesUrl = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=D8vhAXpJCLAebrGLBbCohT84d5UM882W`;
+
+        fetch(articlesUrl)
+            .then(response => response.json())
+            .then(addArticles)
+            .catch(e => requestError(e, 'articles'))
+
+        
+        function addArticles(data) {
+            let htmlContent ='';
+            const articles = data.response.docs;
+            console.log(articles)
+            if(articles && articles.length >= 1) {
+                htmlContent = '<ul>' + articles.map(article =>
+                    `<li class="article">
+                        <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                        <p>${article.snippet}</p>
+                    </li>`
+                ).join('') + '</ul>'
+            } else {
+                htmlContent = 'OPPSSS! There is no articles available';
+            }
+
+            responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+        }
+
+
+        /*** HANDLING NETWORK ERRORS ***/
         function requestError(e, part) {
             console.log(e.message)
 
